ac0f0e952b512f5f90f8604d08886dc26d008f76
ahanaf607307 <ahanaf607307@gmail.com>
2025-01-23 22:59:27 +0600

    compleate signup page

--- src/app/signup/page.jsx
+++ src/app/signup/page.jsx
@@ -3,7 +3,10 @@ import { RegisterLink } from "@kinde-oss/kinde-auth-nextjs/components";
 const SignupPage =async () => {
     return (
         <>
+         <div className=" py-20 flex bg-white text-black flex-col justify-center items-center gap-10">
+
           <RegisterLink postLoginRedirectURL="/login">Sign up</RegisterLink>
+         </div>
         </>
     )
 }
